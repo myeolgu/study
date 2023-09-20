f13bdb1003cb7c6e5ca05495b1130b34c6df4d4f
leejuyeop <juyeop198@etribe.co.kr>
2023-09-20 17:53:17 +0900

    공부용 기록

--- gasp_page/js/main.js
+++ gasp_page/js/main.js
@@ -1,5 +1,7 @@
 gsap.registerPlugin(ScrollTrigger);
 
+gsap.defaults({scrollTrigger: false});
+
 let sections = gsap.utils.toArray(".chapter");
 
 let chapterFirst = gsap.to(sections, {
@@ -7,13 +9,25 @@ let chapterFirst = gsap.to(sections, {
   ease: "none",
   scrollTrigger: {
     trigger: ".container",
+    start: "100 100",
     end: "+=1000",
     scrub: true,
     markers: true,
     pin: true,
   },
+
+  onLeave: () => { 
+    gsap.to('.chapter-2', { xPercent: -80, duration: 2 });
+    gsap.to('.lottehotel-img', { xPercent: -20, duration: 2 });
+  },
+
+  onEnter: () => {
+    gsap.to('.chapter-2', { xPercent: -80, duration: 2 });
+    gsap.to('.lottehotel-img', { xPercent: -20, duration: 2 });
+  }
+
 });
 
-gsap.to('.chapter-2', { xPercent: -80, duration: 2 });
-gsap.to('.lottehotel-img', { xPercent: -20, duration: 2 });
+
+
 
